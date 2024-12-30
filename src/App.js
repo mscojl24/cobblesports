2b23ac34f2bf1311c3ee68c562030b033d2b8d10
jubee <mscojl24@naver.com>
2024-12-30 15:03:17 +0900

    CRA :: 리액트 초기 셋팅

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import './App.css';
+
+function App() {
+  return (
+    <>
+asdasd
+<Figmart>asdasdsad</Figmart>
+    </>
+  );
+}
+
+const Figmart = styled.div`
+  color:#ddd521;
+  text-align: center;
+`;
+
+export default App;
