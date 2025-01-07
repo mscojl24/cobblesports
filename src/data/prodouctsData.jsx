3c122d6b2011b07b912ccee709ffff7134489d2b
jubee <mscojl24@naver.com>
2025-01-07 18:25:49 +0900

    파일업로드

---
+++ src/data/prodouctsData.jsx
@@ -0,0 +1,57 @@
+export const prodouctData = [
+    {
+        id: 1,
+        category: 'watch',
+        title: 'Fenix 8 Series',
+        option: [
+            {
+                name: '카본 그레이 티타늄',
+                size: '51mm',
+                price: '1890000',
+                color: {
+                    colorName: ['카본 그레이 티타늄'],
+                    colorCode: '#000000',
+                },
+                img: {
+                    mainImg: '---',
+                    subImg: ['---', '---'],
+                },
+                display: {
+                    type: 'AMOLED 디스플레이',
+                    touch: true,
+                },
+            },
+        ],
+        spec: {
+            size: '51mm',
+            gps: '멀티밴드 GNSS',
+            memory: '32GB',
+            waterRating: '10ATM',
+            band: '퀵핏 26mm',
+            solar: `Solar 제품한정`,
+        },
+        activityProfiles: [
+            '러닝',
+            '수영',
+            '자전거',
+            '등산',
+            '다이빙(수심40m)',
+            '멀티스포츠',
+            '겨울스포츠',
+            '라켓스포츠',
+            '피트니스',
+            '수상스포츠',
+        ],
+        convenience: {
+            flashlight: true,
+            music: true,
+            Map: true,
+            runningDynamics: true,
+        },
+        battery: {
+            smartwatch: '29일',
+            batterySaver: '41일',
+            gpsOnly: '84시간',
+        },
+    },
+];
