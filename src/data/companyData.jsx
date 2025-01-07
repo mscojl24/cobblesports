5b8601ccbd178558af44ce30d9a440cdd3bc8c77
jubee <mscojl24@naver.com>
2025-01-07 16:51:21 +0900

    📃Docs :: main data 수정

--- src/data/companyData.jsx
+++ src/data/companyData.jsx
@@ -1,26 +1,26 @@
 export const companyData = [
     {
-        category: 'Enjoy',
-        title: '즐기다.',
-        subTitle: '모두의 행복이 될 수 있도록.',
-        image: '../asset/Edge-main-banner.jpg',
+        category: 'Idea',
+        title: '생각하다.',
+        subTitle: '모두에게 도움이 될 수 있도록.',
+        image: '../asset/cobble-business-image-03.png',
+    },
+    {
+        category: 'Achievement',
+        title: '성취하다.',
+        subTitle: '모두와 함께할 수 있도록.',
+        image: '../asset/cobble-business-image-02.png',
+    },
+    {
+        category: 'Communication',
+        title: '소통하다.',
+        subTitle: '모두의 이야기를 담을 수 있도록.',
+        image: '../asset/cobble-business-image-04.png',
     },
     {
         category: 'Enjoy',
         title: '즐기다.',
         subTitle: '모두의 행복이 될 수 있도록.',
-        image: '../asset/Edge-main-banner.jpg',
-    },
-    {
-        category: 'Enjoy',
-        title: '즐기다.',
-        subTitle: '모두의 행복이 될 수 있도록.',
-        image: '../asset/Edge-main-banner.jpg',
-    },
-    {
-        category: 'Enjoy',
-        title: '즐기다.',
-        subTitle: '모두의 행복이 될 수 있도록.',
-        image: '../asset/Edge-main-banner.jpg',
+        image: '../asset/cobble-business-image-01.png',
     },
 ];
--- src/data/swiperMainData.jsx
+++ src/data/swiperMainData.jsx
@@ -1,6 +1,7 @@
 import { BiSearchAlt } from 'react-icons/bi';
 import { BsGrid3X3GapFill } from 'react-icons/bs';
 import { LuPackagePlus } from 'react-icons/lu';
+import { FaYoutube } from 'react-icons/fa';
 
 export const swiperMainData = [
     {
@@ -8,6 +9,7 @@ export const swiperMainData = [
         title: 'Garmin Brand shop',
         subtitle: '공식 제품을 빠르고 쉽게 만나보세요.',
         image: '../asset/Fenix8-main-banner.jpg',
+        video: false,
         linkBtn: {
             text: '스토어 이동하기',
             icon: <BsGrid3X3GapFill size="24px" />,
@@ -18,6 +20,7 @@ export const swiperMainData = [
         title: 'Your own Garmin',
         subtitle: '내가 원하는 제품을 찾아보세요.',
         image: '../asset/Edge-main-banner.jpg',
+        video: false,
         linkBtn: {
             text: '제품 찾아보기',
             icon: <BiSearchAlt size="24px" />,
@@ -25,9 +28,23 @@ export const swiperMainData = [
     },
     {
         index: 3,
+        title: 'Cobble Offical YouTube',
+        subtitle: '코블스포츠 오피셜 유튜브채널',
+        image: '',
+        video: true,
+        videoURL: '../asset/cobble-youtube-video-01.mp4',
+        linkBtn: {
+            text: '유튜브 보러가기',
+            icon: <FaYoutube size="24px" />,
+        },
+    },
+    {
+        index: 4,
         title: 'NEW PRODUCT 2025',
         subtitle: 'Descent X50i 프리미엄 다이브 컴퓨터',
         image: '../asset/X50i-main-banner.jpg',
+        video: true,
+        videoURL: '../asset/Descent_X50i_kr.mp4',
         linkBtn: {
             text: '신제품 확인하기',
             icon: <LuPackagePlus size="24px" />,
