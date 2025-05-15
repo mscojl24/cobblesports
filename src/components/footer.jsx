09b7bb99950fb9f6db7c8f9da2f5a6a3cff7c9fa
jubee <mscojl24@naver.com>
2025-05-15 17:53:03 +0900

    feat :: 프로덕트 데이터 디벨롭 1.0.5v
    ---------------------------------
    - [x]  프로덕트 데이터 마케팅 링크 삽입
    - [x]  feat :: 제품 링크이동 로직 수정 (url 경로)
    - [x]  feat :: 제품 데이터 link / soldout 속성 값 추가
    - [x]  build :: 코드 빌드 후 재 배포 (+데이터 파일)

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,76 +1,88 @@
-import styled from 'styled-components';
-import { BsArrowUpCircle } from 'react-icons/bs';
+import styled from 'styled-components'
+import { BsArrowUpCircle } from 'react-icons/bs'
 
 function Footer() {
-    return (
-        <FooterBox className="flex-v-center">
-            <CompanyInfo className="flex-v-center column">
-                <h1 className="company-name">유한회사 코블스포츠</h1>
-                <div className="information-text flex-v-center column">
-                    <small> 대표자 : 이권표 | 사업자등록번호 : 489-86-02035</small>
-                    <small> 본사주소 : (우)35264 대전광역시 서구 계룡로 599, 102호 | 대표번호 : +82 70-4236-1122</small>
-                    <small> 102, 599 Gyeryong-ro, Seo-gu, Daejeon, 35264, Republic of Korea </small>
-                    <small> 본 사이트의 컨텐츠는 저작권법의 보호를 받는 바 무단 전재, 복사, 배포 등을 금합니다.</small>
-                </div>
-                <h2 className="company-name-en">COBBLE SPORTS Co.,Ltd</h2>
-            </CompanyInfo>
-            <PageUp>
-                <BsArrowUpCircle />
-            </PageUp>
-        </FooterBox>
-    );
+   return (
+      <FooterBox className="flex-v-center">
+         <CompanyInfo className="flex-v-center column">
+            <h1 className="company-name">유한회사 코블스포츠</h1>
+            <div className="information-text flex-v-center column">
+               <small> 대표자 : 박준아 | 사업자등록번호 : 489-86-02035</small>
+               <small>
+                  {' '}
+                  본사주소 : (우)35264 대전광역시 서구 계룡로 599, 102호 |
+                  대표번호 : +82 70-4236-1122
+               </small>
+               <small>
+                  {' '}
+                  102, 599 Gyeryong-ro, Seo-gu, Daejeon, 35264, Republic of
+                  Korea{' '}
+               </small>
+               <small>
+                  {' '}
+                  본 사이트의 컨텐츠는 저작권법의 보호를 받는 바 무단 전재,
+                  복사, 배포 등을 금합니다.
+               </small>
+            </div>
+            <h2 className="company-name-en">COBBLE SPORTS Co.,Ltd</h2>
+         </CompanyInfo>
+         <PageUp>
+            <BsArrowUpCircle />
+         </PageUp>
+      </FooterBox>
+   )
 }
 
-export default Footer;
+export default Footer
 
 const FooterBox = styled.footer`
-    width: 100%;
-    justify-content: space-between;
-    background-color: #000;
-    padding: 100px;
+   width: 100%;
+   justify-content: space-between;
+   background-color: #000;
+   padding: 100px;
 
-    @media (max-width: 860px) {
-        padding: 50px 30px;
-        display: none;
-    }
-`;
+   @media (max-width: 860px) {
+      padding: 50px 30px;
+      display: none;
+   }
+`
 
 const CompanyInfo = styled.div`
-    color: #fff;
-    font-weight: 300;
+   color: #fff;
+   font-weight: 300;
 
-    .company-name {
-        font-size: clamp(16px, 6vw, 22px);
-        margin-bottom: 20px;
-    }
+   .company-name {
+      font-size: clamp(16px, 6vw, 22px);
+      margin-bottom: 20px;
+   }
 
-    .information-text small {
-        font-size: clamp(13px, 5vw, 14px);
-        font-family: '42dot Sans';
-        color: rgba(255, 255, 255, 0.5);
-        margin: 3px 0px;
-        line-height: 1.2;
-    }
+   .information-text small {
+      font-size: clamp(13px, 5vw, 14px);
+      font-family: '42dot Sans';
+      color: rgba(255, 255, 255, 0.5);
+      margin: 3px 0px;
+      line-height: 1.2;
+   }
 
-    .company-name-en {
-        font-size: clamp(20px, 6vw, 40px);
-        font-family: Anton;
-        margin-top: 50px;
-        @media (max-width: 860px) {
-            margin-top: 30px;
-        }
-    }
-`;
+   .company-name-en {
+      font-size: clamp(20px, 6vw, 40px);
+      font-family: Anton;
+      margin-top: 50px;
+      @media (max-width: 860px) {
+         margin-top: 30px;
+      }
+   }
+`
 
 const PageUp = styled.button`
-    font-size: clamp(20px, 6vw, 50px);
-    width: 50px;
-    height: 50px;
-    color: #fff;
-    border: none;
-    cursor: pointer;
+   font-size: clamp(20px, 6vw, 50px);
+   width: 50px;
+   height: 50px;
+   color: #fff;
+   border: none;
+   cursor: pointer;
 
-    @media (max-width: 860px) {
-        display: none;
-    }
-`;
+   @media (max-width: 860px) {
+      display: none;
+   }
+`
--- src/swiper/promotionCard.jsx
+++ src/swiper/promotionCard.jsx
@@ -1,96 +1,122 @@
-import styled from 'styled-components';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import styled from 'styled-components'
+import { Swiper, SwiperSlide } from 'swiper/react'
 
-import 'swiper/css';
-import 'swiper/css/pagination';
-import { Autoplay } from 'swiper/modules';
+import 'swiper/css'
+import 'swiper/css/pagination'
+import { Autoplay } from 'swiper/modules'
 
 function PromotionCard() {
-    const promotion = [
-        {
-            title: '프라이스 파이터 할인 최대 38%',
-            subtitle: '인기 제품을 합리적인 가격으로 만나보세요',
-            image: `/asset/promotion/card-image-01.png`,
-            color: '#000000',
-            fontcolor: '#ff7f29',
-            link: `https://smartstore.naver.com/cobblesports/category/c536631aad7f4e7082e2cc1ec5e790a7?cp=1`,
-        },
-        {
-            title: '가민 챌린저스 운동인증 EVENT',
-            subtitle: '하루하루를 더욱 건강하게',
-            image: `/asset/promotion/card-image-02.png`,
-            color: '#d0ddeb',
-            fontcolor: '#3a5e94',
-            link: `https://cafe.naver.com/cobblesports/3261`,
-        },
-        {
-            title: '우리, 오늘부터 친구할래?',
-            subtitle: '코블스포츠 카카오톡 채널 친구추가 이벤트',
-            image: `/asset/promotion/card-image-03.png`,
-            color: '#d0e8eb',
-            fontcolor: '#1f5363',
-            link: `https://www.instagram.com/p/DGDH7txpQ1i/`,
-        },
-    ];
+   const promotion = [
+      {
+         title: '📦 코블스포츠 구매인증 이벤트',
+         subtitle: '구매 인증하고 포러너 265 받아가자!',
+         color: '#000000',
+         fontcolor: '#c4c4c4',
+         link: `https://cafe.naver.com/cobblesports/6472`,
+      },
+      {
+         title: '🌠 코블스포츠 가입 이벤트',
+         subtitle: '코블스포츠 가입하면 굿즈가 펑펑~',
+         color: '#ffc559',
+         fontcolor: '#5e2a00',
+         link: `https://cafe.naver.com/cobblesports/7177`,
+      },
+      {
+         title: '🗨️ 코블스포츠 유튜브 댓글 이벤트 ①',
+         subtitle: '비보액티브 6 출시기념',
+         color: '#d0e8eb',
+         fontcolor: '#1f5363',
+         link: `https://cafe.naver.com/cobblesports/6222`,
+      },
+      {
+         title: '🗨️ 코블스포츠 유튜브 댓글 이벤트 ②',
+         subtitle: '인스팅트3 택티컬 출시기념',
+         color: '#000000',
+         fontcolor: '#789b41',
+         link: `https://cafe.naver.com/cobblesports/6699`,
+      },
+      {
+         title: '💰 프라이스 파이터 할인 최대 38%',
+         subtitle: '인기 제품을 합리적인 가격으로 만나보세요',
+         color: '#000000',
+         fontcolor: '#ff7f29',
+         link: `https://smartstore.naver.com/cobblesports/category/c536631aad7f4e7082e2cc1ec5e790a7?cp=1`,
+      },
+      {
+         title: '👟 가민 챌린저스 운동인증 EVENT',
+         subtitle: '하루하루를 더욱 건강하게',
+         color: '#d0ddeb',
+         fontcolor: '#3a5e94',
+         link: `https://cafe.naver.com/cobblesports/5078`,
+      },
+   ]
 
-    return (
-        <>
-            <Swiper
-                spaceBetween={0}
-                autoplay={{
-                    delay: 4000,
-                    disableOnInteraction: false,
-                }}
-                loop={true}
-                modules={[Autoplay]}
-                className="mySwiper">
-                {promotion.map((item, index) => (
-                    <SwiperSlide key={index}>
-                        <Promotion
-                            className="flex-center column"
-                            color={item.color}
-                            $fontcolor={item.fontcolor}
-                            onClick={() => {
-                                window.open(item.link, '_blank', 'noopener,noreferrer');
-                            }}>
-                            <p className="promotion-sub">{item.subtitle}</p>
-                            <h1 className="promotion-title">{item.title}</h1>
-                        </Promotion>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-        </>
-    );
+   return (
+      <>
+         <Swiper
+            spaceBetween={0}
+            autoplay={{
+               delay: 4000,
+               disableOnInteraction: false,
+            }}
+            loop={true}
+            modules={[Autoplay]}
+            className="mySwiper"
+         >
+            {promotion.map((item, index) => (
+               <SwiperSlide key={index}>
+                  <Promotion
+                     className="flex-center column"
+                     color={item.color}
+                     $fontcolor={item.fontcolor}
+                     onClick={() => {
+                        window.open(item.link, '_blank', 'noopener,noreferrer')
+                     }}
+                  >
+                     <p className="promotion-sub">{item.subtitle}</p>
+                     <h1 className="promotion-title">{item.title}</h1>
+                  </Promotion>
+               </SwiperSlide>
+            ))}
+         </Swiper>
+      </>
+   )
 }
 
 const Promotion = styled.article`
-    position: relative;
-    width: 100%;
-    height: 200px;
-    padding: 100px;
-    background: linear-gradient(160deg, ${(props) => props.color} 50%, ${(props) => props.$fontcolor} 180%);
-    transition: all 0.3s ease-in-out;
-    cursor: pointer;
+   position: relative;
+   width: 100%;
+   height: 200px;
+   padding: 100px;
+   background: linear-gradient(
+      160deg,
+      ${(props) => props.color} 50%,
+      ${(props) => props.$fontcolor} 180%
+   );
+   transition: all 0.3s ease-in-out;
+   cursor: pointer;
 
-    & > * {
-        margin: 7px 0px;
-        color: ${(props) => props.$fontcolor};
-        text-align: center;
-    }
+   & > * {
+      margin: 7px 0px;
+      color: ${(props) => props.$fontcolor};
+      text-align: center;
+   }
 
-    .promotion-sub {
-        font-size: clamp(18px, 4vw, 25px);
-    }
+   .promotion-sub {
+      font-family: '42dot Sans';
+      font-size: clamp(18px, 4vw, 25px);
+   }
 
-    .promotion-title {
-        font-size: clamp(24px, 6vw, 36px);
-        font-weight: 700;
-    }
+   .promotion-title {
+      font-family: '42dot Sans';
+      font-size: clamp(24px, 6vw, 36px);
+      font-weight: 700;
+   }
 
-    @media (max-width: 860px) {
-        height: 150px;
-        padding: 30px;
-    }
-`;
+   @media (max-width: 860px) {
+      height: 150px;
+      padding: 30px;
+   }
+`
 
-export default PromotionCard;
+export default PromotionCard
